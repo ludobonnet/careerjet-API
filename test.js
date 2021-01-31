3d10ad8436029e03b07c4c06c8100aa644c77e6e
Trevor Ewen <me@trevorewe.com>
2021-01-31 11:05:28 -0500

    Added the contract type parameter and updated the test configuration.

--- test.js
+++ test.js
@@ -1,18 +1,28 @@
-var careerjet = require('./index.js');
+require('dotenv').config();
 
-const careerjetAPI = new careerjet({locale:'en_GB', affid: 'youraffidkey', user_ip: '127.0.0.1', user_agent: 'internetexplorer'});
+const careerjet = require('./index.js');
+
+const careerjetAPI = new careerjet({
+  locale: 'en_US',
+  affid: process.env.CAREERJET_AFFILIATE_ID,
+  user_ip: process.env.USER_IP,
+  user_agent: process.env.USER_AGENT
+});
 
 careerjetAPI
-    .location('London')
-    .radius(1)
-    .page(2)
-    .sortBy('date')
-    .query(
-        function (results) {
-            // do something with the success results
-            console.log(results);
-        },
-        function (error) {
-            // do something with the error results
-            console.error(error);
-      });
+ /* .location('New York') */
+  .contractType('c')
+  .keywords('Software Engineer')
+  .radius(1)
+  .pagesize(50)
+  .page(2)
+  .sortBy('date')
+  .query(
+    (results) => {
+      // do something with the success results
+      console.log(results);
+    },
+    (error) => {
+      // do something with the error results
+      console.error(error);
+    });
